683e6b8df1c242507ba8115ad099dbf1f7188dc7
POO <angel.villamil@epn.edu.ec>
2024-06-13 09:25:32 -0500

    Creacion CLIENTE

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,25 @@
-public class Clientes {
+public class Cliente {
+    public String id_cliente;
+    public String nombre;
+
+    public Cliente(String id_cliente, String nombre) {
+        this.id_cliente = id_cliente;
+        this.nombre = nombre;
+    }
+
+    public String getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(String id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
