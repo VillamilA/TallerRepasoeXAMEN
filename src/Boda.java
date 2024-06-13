91f0775a7f7ee80757e69faf2c51db54e1f5fe7e
POO <angel.villamil@epn.edu.ec>
2024-06-13 09:27:35 -0500

    Creacion hija TipoEvento Boda

---
+++ src/Boda.java
@@ -0,0 +1,33 @@
+public class Boda extends TipoEvento{
+    public String fecha;
+    public String lugar;
+
+    public Boda(String id_cliente, String fecha, String tipo_evento, Double costo_base, String fecha1, String lugar) {
+        super(id_cliente, fecha, tipo_evento, costo_base);
+        this.fecha = fecha1;
+        this.lugar = lugar;
+    }
+
+    @Override
+    public String getFecha() {
+        return fecha;
+    }
+
+    @Override
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    @Override
+    public double Calculo_costo() {
+        return costo_base+1000;
+    }
+}
